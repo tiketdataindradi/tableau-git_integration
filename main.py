3b92659875d6441582d64265708a29eb4eefd5d7
tiketdataAdi <indradi.maulana@tiket.com>
2022-08-29 10:41:41 +0700

    upload salah nama di metadata

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def main(args):
                     status = False
             else:
                 logging.info(f"Skip publishing workbook: { file } not listed in config files")
-                list_message.append(f"Skip publishing workbook: { file.split('.')[0]}, because not listed in config files  :x:")
+                list_message.append(f"Skip publishing workbook: { file.split('.')[0]}, because not listed in config files or files in .twb format :x:")
                 list_message.append("Make sure workbook name in config file (workbooks.yml) is correct and uploaded file is in .twbx format")
                 status = False
 
@@ -170,7 +170,7 @@ def main(args):
     else:
         logging.info("No file changes detected")
         list_message.append("No file changes detected")
-        list_message.append("Make sure workbook name in config file (workbooks.yml) is correct and uploaded file is in .twbx format")
+        list_message.append("Make sure to upload your updated file, workbook name in config file (workbooks.yml) is correct and uploaded file is in .twbx format")
         comment_pr(args.repo_token, "\n".join(list_message))
         sys.exit(1)
     sys.exit(0)
